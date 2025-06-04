7f929546e80105663709065638189706050ba211
danielzipx <danielsuhett11@gmail.com>
2025-06-03 22:10:19 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,22 +9,24 @@ roleta_russa = ''
 
 for i in range(0,5):
     roleta_russa = input('Quer jogar? S/N: ')
-    if roleta_russa == 'N':
-        print('Por que você está aqui ainda então?')
-        break
-
-    if roleta_russa == 'S':
-        roleta_russa = random.randint(1,6)
-        
-        if roleta_russa == numero_secreto:
-            print('deletando, \nC:\Windows\System32')
-            print('tchauzinho...')
+    if roleta_russa == '':
+        print('carregando...')
+        if roleta_russa == 'N':
+            print('Por que você está aqui ainda então?')
             break
-        
-        if roleta_russa != numero_secreto:
-            print('Você sobreviveu tente novamente...')    
-        else:
-            print('Não foi dessa vez :( \nNa proxima eu sei que você consegue :)')
+            
+        if roleta_russa == 'S':
+            roleta_russa = random.randint(1,6)
+            
+            if roleta_russa == numero_secreto:
+                print('deletando, \nC:\Windows\System32')
+                print('tchauzinho...')
+                break
+            
+            if roleta_russa != numero_secreto:
+                print('Você sobreviveu tente novamente...')    
+    else:
+        print('Não foi dessa vez :( \nNa proxima eu sei que você consegue :)')
 
 
 
